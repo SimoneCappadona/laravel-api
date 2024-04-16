1ca1d53a79506f944bebb30c568c66bd15fa6a6a
SimoneCappadona <simocappa95@gmail.com>
2024-04-16 18:24:10 +0200

    feat:

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TypeSeeder extends Seeder
 {
@@ -12,8 +14,19 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+
+
+    public function run(Faker $faker)
     {
-        //
+        $type_names = ['Front-end', 'Back-end', 'FullStack'];
+
+
+        foreach ($type_names as $typename) {
+            $type = new Type;
+            $type->label = $typename;
+            $type->color = $faker->hexColor();
+            $type->save();
+        }
+
     }
 }
